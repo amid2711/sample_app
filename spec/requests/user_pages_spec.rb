22f51085189dbc2328c26b00734dc85d042fcad4
Vyalkov Dmitry <amid1978@ukr.net>
2014-07-02 13:44:10 +0400

    Chapter 8, after exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,12 +25,7 @@ describe "UserPages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { valid_user }
 
       it "should create a user" do
         expect { click_button submit}.to change(User, :count).by(1)
@@ -42,7 +37,7 @@ describe "UserPages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+        it { should have_success_message('Welcome')}
 
         describe "follow by signout" do
           before { click_link "Sign out" }
